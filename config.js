26390818782d8fd1605035441b0cda42142bb507
Mdashik-pradhan <mdashikpradhan75@gmail.com>
2021-06-16 17:31:17 +0600

    email and password validate

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://chatBuzz:LEWZ9fOtPuwePGwV@cluster0.hda5d.mongodb.net/merng?retryWrites=true',
+    SECRET_KEY: 'some very secrete key'
+}
--- indes.js
+++
@@ -1,1 +0,0 @@
-const Ashik = 'hello'
